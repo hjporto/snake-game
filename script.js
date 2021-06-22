46f98ca7fa813d100c7738b810221df557791809
Hugo Porto <hugoporto.ti@gmail.com>
2021-06-22 02:25:13 -0300

    criação do alimento para a cobrinha

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let box = 32;
 let snake = [];
 snake[0] = { x: 8 * box, y: 8 * box };
 let direction = "right";
+let food = {
+	x: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box,
+}
 
 
 function criarBG() {
@@ -16,10 +20,28 @@ function criarCobrinha() {
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
+function drawFood() {
+	context.fillStyle = "red";
+	context.fillRect(food.x, food.y, box, box);
+}
+
+document.addEventListener('keydown', update);
+
+function update(event) {
+	if (event.keycode == 37 && direction != "right") direction = "left";
+	if (event.keycode == 38 && direction != "down") direction = "up";
+	if (event.keycode == 39 && direction != "left") direction = "right";
+	if (event.keycode == 40 && direction != "up") direction = "down";
+}
 
 function iniciarJogo() {
+	if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+	if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+	if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+	if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 	criarBG();
 	criarCobrinha();
+	drawFood();
 
 	let snakeX = snake[0].x;
 	let snakeY = snake[0].y;
@@ -36,6 +58,6 @@ function iniciarJogo() {
 		y: snakeY
 	}
 	snake.unshift(newHead);
+}
 
-
-	let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 100);
