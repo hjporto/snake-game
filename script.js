340e870d7b6296c83f0793a87c2d87063b4d7b49
Hugo Porto <hugoporto.ti@gmail.com>
2021-06-22 03:42:45 -0300

    Adição de estilo e correção de escrita em html e JS

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
-snake[0] = { x: 8 * box, y: 8 * box };
+snake[0] = {
+	x: 8 * box,
+	y: 8 * box
+}
 let direction = "right";
 let food = {
 	x: Math.floor(Math.random() * 15 + 1) * box,
-	y: Math.floor(Math.random() * 15 + 1) * box,
+	y: Math.floor(Math.random() * 15 + 1) * box
 }
 
 
@@ -25,7 +28,7 @@ function drawFood() {
 	context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
+document.addEventListener("keydown", update);
 
 function update(event) {
 	if (event.keycode == 37 && direction != "right") direction = "left";
@@ -39,6 +42,14 @@ function iniciarJogo() {
 	if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 	if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
 	if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+	for (i = 1; i < snake.length; i++) {
+		if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+			clearInterval(jogo);
+			alert("Game over");
+
+		}
+	}
 	criarBG();
 	criarCobrinha();
 	drawFood();
